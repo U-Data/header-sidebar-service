463b54fbadc8f064b81afe0ae2acba8033443685
Angela Zhou <angelazhou922@gmail.com>
2018-11-30 16:27:50 -0800

    working react

--- sdc/server/index.js
+++ sdc/server/index.js
@@ -2,29 +2,30 @@ const newRelic = require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const courses = require('../postgres/index.js');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3003;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/:id', express.static('../../public/dist'));
+app.use('/courses/:id', express.static(path.join(__dirname, '../../public/dist')));
 
 app.get('/healthCheck', (req, res) => {
   res.status(200).send('health check ok');
 });
 
-app.get('/courses', (req, res) => {
-  courses.getAllCourses((err, results) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(results);
-    }
-  });
-});
+// app.get('/courses', (req, res) => {
+//   courses.getAllCourses((err, results) => {
+//     if (err) {
+//       res.status(500).send(err);
+//     } else {
+//       res.status(200).send(results);
+//     }
+//   });
+// });
 
-app.get('/courses/:id', (req, res) => {
+app.get('/courses/:id/header', (req, res) => {
   courses.getASingleCourse(req.params.id, (err, result) => {
     if (err) {
       res.status(500).send(err);
